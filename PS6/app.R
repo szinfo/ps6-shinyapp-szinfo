69c17d3970d1a0f7d316ceafcab58c09f9f7c581
szinfo <szinbarg@uw.edu>
2023-03-04 23:23:11 -0800

    working through it

--- PS6/app.R
+++ PS6/app.R
@@ -2,12 +2,26 @@ library(shiny)
 library(tidyverse)
 library(ggplot2)
 
-overdose_data <- read.csv("overdose-by-demographic.csv")
+overdose_data<- read_delim("overdose-by-demographic.csv")
 
-ui <- fluidpage(
-  
+ui <- fluidPage(
+  titlePanel("About"),
+  sidebarLayout(
+    sidebarPanel(
+      p("Here you can analyze the drug overdose rates by demographics such as 
+      Sex, and race as well as a breakdown of overdoses by drug
+      The dataset comes from the CDC, containing the rates of overdoses by 
+      year from 1999 to 2021
+      Here is a table of overall rates of overdose regardless of drug from
+        1999 to 2021, broken down by sex")
+    ),
+    mainPanel(
+      tableOutput("Overdose_Total")
+    )
+  )
 )
-
+server <- function(input, output) {
+  output$Overdose_Total <- renderTable({overdose_data %>% slice(6:9)}) 
+}
 
 shinyApp(ui = ui, server = server)
-
