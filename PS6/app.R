77619f1997ed66ade0650d98d5b09d4331c020ee
szinfo <szinbarg@uw.edu>
2023-03-05 17:35:37 -0800

    done with first page

--- PS6/app.R
+++ PS6/app.R
@@ -2,26 +2,31 @@ library(shiny)
 library(tidyverse)
 library(ggplot2)
 
-overdose_data<- read_delim("overdose-by-demographic.csv")
+overdose_data<- read_delim("Overdose-demographic-data.csv")
 
-ui <- fluidPage(
-  titlePanel("About"),
-  sidebarLayout(
-    sidebarPanel(
-      p("Here you can analyze the drug overdose rates by demographics such as 
-      Sex, and race as well as a breakdown of overdoses by drug
-      The dataset comes from the CDC, containing the rates of overdoses by 
-      year from 1999 to 2021
-      Here is a table of overall rates of overdose regardless of drug from
-        1999 to 2021, broken down by sex")
-    ),
-    mainPanel(
-      tableOutput("Overdose_Total")
-    )
-  )
+ui <- navbarPage("Drug Overdose Data Analysis",
+                 tabPanel("About",
+                          sidebarLayout(
+                            sidebarPanel(
+                              p("Here you can analyze the drug overdose rates 
+                              per 100,000 by demographics such as sex, and 
+                              ethincity as well as a breakdown of overdoses by 
+                              drug. The dataset comes from the CDC, containing 
+                              the rates of overdoses by year from 1999 to 2021. 
+                              Here is a quick look of overdose rates overall 
+                              then broken down by sex")
+                            ), mainPanel(
+                              tableOutput("Overdose_Total")
+                            ))),
+  tabPanel("Plots",
+           ),
+  tabPanel("Tables",
+           ),
 )
+
+
 server <- function(input, output) {
-  output$Overdose_Total <- renderTable({overdose_data %>% slice(6:9)}) 
+  output$Overdose_Total <- renderTable({overdose_data %>%  slice(1:3) })
 }
 
 shinyApp(ui = ui, server = server)
