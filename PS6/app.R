37f5adc27fae973e80b303b182d2f298d34b0f24
szinfo <szinbarg@uw.edu>
2023-03-06 23:16:22 -0800

    almost done

--- PS6/app.R
+++ PS6/app.R
@@ -2,10 +2,11 @@ library(shiny)
 library(tidyverse)
 library(ggplot2)
 
-
 weather_data <- read_delim("UAH-lower-troposphere-long.csv")
 
-if(interactive()) {
+weather_data_2<- weather_data %>% 
+  mutate(time = (year+(month/12)))
+
 ui <- navbarPage(
   "Temperature Data Analysis",
   tabPanel(
@@ -28,8 +29,8 @@ ui <- navbarPage(
       sidebarPanel(
         p("With this graph you can analyze the temperature by different regions.
           Select the region you wish to observe. The scatterplot shows the data
-          by year, with multiple points at each year for the different months."
-          ),
+          by month."
+        ),
         radioButtons("marker", "Choose marker option:",
                      c("EXPR", "Option_2", "Option_3")),
         uiOutput("Region")
@@ -37,49 +38,68 @@ ui <- navbarPage(
       mainPanel(
         plotOutput("temp_scatterplot"),
         textOutput("observation")
+      )
     )
-  )
-),
+  ),
   tabPanel(
     "Tables",
     sidebarLayout(
       sidebarPanel(
         p("The data table to the right shows the average temp across all regions
           over different years"),
-        uiOutput("Time")),
+        uiOutput("Time")
+      ),
       mainPanel(
         textOutput("Temp_range"),
-        plotOutput("temp_scatterplot")
+        tableOutput("Temp_by_year")
       )
     )
   )
 )
 
 server <- function(input, output) {
-  output$temp_random <- renderTable({weather_data %>% sample_n(5)})
+  output$temp_random <- renderTable({weather_data_2 %>% sample_n(5)})
   output$Region <- renderUI ({
-  checkboxGroupInput("region",
-                      "Select the region to display:",
-                      choices = (unique(weather_data$region )))
-})
-output$temp_scatterplot <- renderPlot({
-  subset <- weather_data %>% 
-    filter(region %in% input$region)
-  marker <- switch(input$marker,
-                   "EXPR" = 19,
-                   "Option_2" = 6,
-                   "Option_3" = 12)
-    ggplot(data = subset, aes(x = year, y = temp, color = subset$region)) + 
+    checkboxGroupInput("region",
+                       "Select the region to display:",
+                       choices = (unique(weather_data_2$region )))
+    })
+  output$temp_scatterplot <- renderPlot({
+    subset <- weather_data_2 %>% 
+      filter(region %in% input$region)
+    marker <- switch(input$marker,
+                     "EXPR" = 19,
+                     "Option_2" = 6,
+                     "Option_3" = 12)
+    ggplot(data = subset, aes(x = time, y = temp, color = region)) + 
       geom_point(shape = marker) 
-      })
-output$observation <- renderText({
-  text_data <- weather_data %>% 
-    filter(region %in% input$region)
-  paste("You are currently viewing this region: ", 
-         unique(text_data$region))
-})
-
-  }
+    })
+  output$observation <- renderText({
+    text_data <- weather_data_2 %>% 
+      filter(region %in% input$region)
+    paste0("There are ", 
+          count(text_data), " obeservations.")
+  })
+  output$Time <- renderUI ({
+      checkboxGroupInput("year",
+                         "Select the year(s) you wish to observe",
+                         choices = (unique(weather_data_2$year)))
+  })
+  output$Temp_by_year <- renderTable({
+      table_data <- weather_data_2 %>% 
+        group_by(year) %>%
+        mutate(ave_temp = mean(temp)) %>%
+        distinct(ave_temp) %>% 
+        filter(year %in% input$year) 
+        
+        
+  })
+  output$Temp_range <- renderText({
+    text_data <- weather_data_2 %>% 
+      group_by(year)
+    paste0("you are currently looking at the year ", unique(text_data$year))
+  })
+}
 
 shinyApp(ui = ui, server = server)
-}
+
